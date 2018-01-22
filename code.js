553f6f1c7ba6f48ddb93c5c5e75628c77543e3a2
cygas <cygas7@gmail.com>
2018-01-22 22:06:28 +0100

    changes

--- code.js
+++ code.js
@@ -1,2 +1,15 @@
-window.addEventListener();
+window.addEventListener("load", function(){
+	
+	const canvas = document.getElementById("canvas");
+	const context = canvas.getContext("2d");
+	const cvsw = canvas.width;
+	const cvsh = canvas.height;
+	const PI = Math.PI;
+	const UP = 38;
+	const DOWN = 40;
+	
+	
+	
+	
+});
 
